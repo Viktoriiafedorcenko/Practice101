d93de1fef6fe5170bf3e1355a6abbc6d23f9f401
Your Name <you@example.com>
2023-10-14 10:11:37 +0300

    first commit

---
+++ app/src/main/java/com/example/practicebutton_10/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.practicebutton_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun Click(view: View) {
+            val button1 = findViewById<Button>(R.id.button1)
+            val button2 = findViewById<Button>(R.id.button2)
+
+            button1.setOnClickListener{
+                button2.visibility = View.VISIBLE
+                button1.visibility = View.INVISIBLE
+            }
+            button2.setOnClickListener{
+                button1.visibility = View.VISIBLE
+                button2.visibility = View.INVISIBLE
+            }
+
+        }
+}
+
+
